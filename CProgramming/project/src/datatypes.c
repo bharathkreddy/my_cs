c01ee35b76a3a67556863970c0bbc3c9a75909a5
Bharath k Reddy <reddybharath.k@gmail.com>
2025-08-18 12:11:43 +0100

    move to linux machine

--- CProgramming/project/src/datatypes.c
+++ CProgramming/project/src/datatypes.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-    int x = 42;
-    int *p = &x;
+    float weather[5][12] = {
+        {1.1, 1.2, 3, 4, 5.5, 6, 7, 8, 9, 10, 11, 12.5},
+        {2, 1, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12},
+        {12,34,32,4,5,6,8,43,5,6,7,8},
+        {12,34,32,4,5,6,8,43,5,6,7,8},
+        {12,34,32,4,5,6,8,43,5,6,7,20},
+    };
 
-    printf("value of x: %d\n", x);
-    printf("address of x: %d\n", &x);
-    printf("value of p: %d\n", p);
-    printf("address of p: %d\n", &p);
-    printf("value at address in p: %d\n", *p);
-
+    for(int i=0; i<5; ++i){
+        float sum = 0.0;
+        for(int j=0; j<12; ++j){
+            sum += weather[i][j];
+        }
+        printf("Avg rainfall for year %d: %f\n", i+2015, sum);
+    }
     return 0;
 }
+
